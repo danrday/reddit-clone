be05ba024f02d6840c239a823ab4f82780aaab68
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-09-15 11:13:18 -0500

    routes should be working

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const routes = require('./routes/')
 const {connect} = require('./database')
+const { cyan, red } = require('chalk')
 const bodyParser = require('body-parser')
 
 const app=express()
+app.set('view engine','pug')
 
+
+//middleware
+app.use(({ method, url, headers: { 'user-agent': agent } }, res, next) => {
+  const timeStamp = new Date()
+  console.log(`[${timeStamp}] "${cyan(`${method} ${url}`)}" "${agent}"`)
+  next()
+})
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(routes)
 app.listen(3000)
