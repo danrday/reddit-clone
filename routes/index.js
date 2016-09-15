0b9cb65a5dd60cd86b1553514f04673f20828eaf
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-09-15 14:57:01 -0500

    upvotes saving to mongo

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const {Router} = require('express');
-const postModel = require('../models/post.js')
+const postModel = require('../models/post')
+const upvote = require('../models/upvote')
 
 const router = Router()
 router.get('/', (req, res) =>
-  postModel.find()
-  .then(data=>res.render('index',{data}))
-
+	Promise.all([
+		postModel.find(),
+		upvote.find()]).
+	then(([data,upvotes])=>{
+		// Object.keys(upvotes).forEach((vote)=>{
+		// 	console.log(upvotes[vote])
+		// })
+		res.render('index',{data,upvotes})
+	})
   
 )
 
@@ -19,5 +26,21 @@ router.post('/post', (req, res, err) => {
   .then(res.redirect('/'))
   .catch(err=>console.log(err))
 })
+
+router.post('/', (req, res, err) => {
+	let dataArray;
+	let data = req.body.upvote
+	data=data.slice(1,data.length-1)
+	dataArray=data.split(',')
+	let upvoteObj=
+	{
+		id:dataArray[1],
+		upvote:dataArray[0]
+	}
+	const vote = new upvote(upvoteObj);
+	vote.save();
+})
+
+
 
 module.exports=router;
